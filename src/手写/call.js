a5db4126309f8f7340d75d8efdb32cfc2a1748b3
DaphnisLi <1872758972@qq.com>
2022-11-23 17:18:04 +0800

    feat: call

---
+++ src/手写/call.js
@@ -0,0 +1,17 @@
+// 将函数设为对象的属性
+// 执行该函数
+// 删除该函数
+// 多个参数
+// this（指的是传入的 obj） 可能是 null
+// this 可以设置 fn
+/**
+ * @param {*} context 上下文 this
+ */
+Function.prototype.myCall = function (context) {
+  const currentContext = context || window
+  currentContext.fn = this
+  const params = arguments.length > 1 ? [...arguments].slice(1) : []
+  const fnReturn = currentContext.fn(...params)
+  delete currentContext.fn
+  return fnReturn
+}
