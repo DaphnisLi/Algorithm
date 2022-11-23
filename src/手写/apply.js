8ab358a76c46d94dd53f124b29b8575564e2750b
DaphnisLi <1872758972@qq.com>
2022-11-23 17:56:40 +0800

    feat: apply

---
+++ src/手写/apply.js
@@ -0,0 +1,15 @@
+// 和 call 的区别: call 的参数是一个个传的, 但是 apply 的参数是全放在数组里, 然后再挨个传给 fn
+
+/**
+ *
+ * @param {*} context 上下文 this
+ * @param {*} fnParam 函数的参数
+ * @returns
+ */
+Function.prototype.myApply = function (context, fnParam) {
+  const currentContext = context || window
+  currentContext.fn = this
+  const fnReturn = currentContext.fn(...fnParam)
+  delete currentContext.fn
+  return fnReturn
+}
