57ece70c543caf642d3dd7ed380f03e35b707e10
DaphnisLi <1872758972@qq.com>
2022-12-09 11:32:31 +0800

    feat: 函数柯里化

---
+++ src/工具函数/函数柯里化.js
@@ -0,0 +1,23 @@
+// 函数柯里化
+const curry = function (fn) {
+  const params = [...arguments].slice(1)
+  const fnParamsLength = fn.length
+  return function () {
+    const _params = [...params, ...arguments]
+
+    if (fnParamsLength > _params.length) {
+      return curry.call(this, fn, ..._params)
+    } else {
+      return fn.apply(this, _params)
+    }
+  }
+}
+
+const fn = curry((a, b, c) => {
+  console.log([a, b, c])
+})
+
+fn(1, 2, 3)
+fn(1, 2)(3)
+fn(1)(2)(3)
+fn(1)(2, 3)
