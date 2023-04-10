ee81eb6eb94b4a9ae292d5f881390c8571eb74b0
DaphnisLi <1872758972@qq.com>
2023-04-10 21:09:49 +0800

    feat: 数组

--- src/LeetCode/数组/27.移除元素.js
+++ src/LeetCode/数组/27.移除元素.js
@@ -4,14 +4,26 @@
  * [27] 移除元素
  */
 
+// 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+
+// 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+
+// 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
 // @lc code=start
 /**
  * @param {number[]} nums
  * @param {number} val
  * @return {number}
  */
-var removeElement = function(nums, val) {
-
-};
+var removeElement = function (nums, val) {
+  let slow = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[slow] = nums[i]
+      slow++
+    }
+  }
+  return slow + 1
+}
 // @lc code=end
-
