ec710c8e5fc47fca7ae07e5730ae6755af9bd6cf
DaphnisLi <1872758972@qq.com>
2023-05-12 11:30:02 +0800

    feat: 对象扁平化

---
+++ src/面试题/对象扁平化.js
@@ -0,0 +1,57 @@
+const obj = {
+  a: {
+    b: 1,
+    c: 2,
+    d: { e: 5 },
+  },
+  b: [1, 3, { a: 2, b: 3 }],
+  c: 3,
+}
+
+const flatten = (res, obj, keyName) => {
+  if (!obj) return
+  if (obj instanceof Array) {
+    obj.forEach((item, index) => {
+      flatten(res, item, `${keyName}[${index}]`)
+    })
+  } else if (obj instanceof Object) {
+    for (const key in obj) {
+      if (obj[key] instanceof Object) {
+        flatten(res, obj[key], `${keyName}.${key}`)
+      } else if (obj[key] instanceof Array) {
+        flatten(res, obj[key], `${keyName}.${key}`)
+      } else {
+        res[`${keyName}.${key}`] = obj[key]
+      }
+    }
+  } else {
+    res[keyName] = obj
+  }
+}
+
+const main = (obj) => {
+  const res = {}
+  for (const key in obj) {
+    if (obj[key] instanceof Array) {
+      flatten(res, obj[key], `${key}`)
+    } else if (obj[key] instanceof Object) {
+      flatten(res, obj[key], `${key}`)
+    } else {
+      res[key] = obj[key]
+    }
+  }
+  return res
+}
+
+console.log(main(obj))
+
+// {
+//   'a.b': 1,
+//   'a.c': 2,
+//   'a.d.e': 5,
+//   'b[0]': 1,
+//   'b[1]': 3,
+//   'b[2].a': 2,
+//   'b[2].b': 3,
+//   c: 3,
+// }
